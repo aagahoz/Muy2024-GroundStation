6da85709ad07f091df87b74748346d287d0b1d37
Agah Ozdemir <ozdemiragah@gmail.com>
2024-04-26 12:22:18 +0300

    Some New Features for GUI Design

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QComboBox, QTableView, QHeaderView, QLabel
-from PyQt6.QtGui import QFont, QStandardItemModel, QStandardItem, QPixmap
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QComboBox, QTableView, QHeaderView, QLabel, QCheckBox
+from PyQt6.QtGui import QFont, QStandardItemModel, QStandardItem, QPixmap, QIcon, QColor
 import pyqtgraph as pg
 from PyQt6.QtCore import Qt, QTime, QTimer, QUrl
+from PyQt6 import QtCore, QtWidgets
 
 
 class MainWindow(QMainWindow):
@@ -12,6 +13,8 @@ class MainWindow(QMainWindow):
         self.setFixedHeight(900)
         self.setFixedWidth(1720)
         self.move(100, 50)  # Pencereyi (100, 50) konumunda başlatır
+
+        self.setWindowIcon(QIcon("Taslak\Logo-Icon.ico"))
 
         self.init_ui()
 
@@ -65,25 +68,25 @@ class MainWindow(QMainWindow):
         self.BaudSelector.setFont(QFont("Times New Roman", 12))
 
         self.FirstTimeSelector = QComboBox(self)
-        self.FirstTimeSelector.setGeometry(1520, 50, 40, 32)
+        self.FirstTimeSelector.setGeometry(1510, 50, 50, 32)
         self.FirstTimeSelector.addItem("1")
         self.FirstTimeSelector.addItem("2")
         self.FirstTimeSelector.setFont(QFont("Times New Roman", 12))
 
         self.FirstColorSelector = QComboBox(self)
-        self.FirstColorSelector.setGeometry(1560, 50, 40, 32)
+        self.FirstColorSelector.setGeometry(1560, 50, 50, 32)
         self.FirstColorSelector.addItem("R")
         self.FirstColorSelector.addItem("G")
         self.FirstColorSelector.setFont(QFont("Times New Roman", 12))
 
         self.SecondTimeSelector = QComboBox(self)
-        self.SecondTimeSelector.setGeometry(1600, 50, 40, 32)
+        self.SecondTimeSelector.setGeometry(1610, 50, 50, 32)
         self.SecondTimeSelector.addItem("1")
         self.SecondTimeSelector.addItem("2")
         self.SecondTimeSelector.setFont(QFont("Times New Roman", 12))
 
         self.SecondColorSelector = QComboBox(self)
-        self.SecondColorSelector.setGeometry(1640, 50, 40, 32)
+        self.SecondColorSelector.setGeometry(1660, 50, 50, 32)
         self.SecondColorSelector.addItem("R")
         self.SecondColorSelector.addItem("G")
         self.SecondColorSelector.setFont(QFont("Times New Roman", 12))
@@ -111,10 +114,57 @@ class MainWindow(QMainWindow):
         self.MekanikFiltrelemeLabel.setText(MekanikFiltrelemeString)
         self.MekanikFiltrelemeLabel.setStyleSheet("background-color: white")
 
+        # add progress bar
+        self.progressBar = QtWidgets.QProgressBar(self)
+        self.progressBar.setGeometry(QtCore.QRect(1000, 20, 250, 40))
+        self.progressBar.setProperty("value", 34)
+        self.progressBar.setObjectName("progressBar")
+        self.progressBar.setValue(0)
+        self.progressBar.setMaximum(100)
+        self.progressBar.setMinimum(0)
+
+        # Checkbox ekle
+        self.checkbox1 = QCheckBox('0 : Uçuşa Hazır', self)
+        self.checkbox1.setGeometry(1030, 68, 170, 40)
+        self.checkbox1.setFont(QFont("Times New Roman", 14))
+        self.checkbox1.setChecked(True)  # Checkbox'u varsayılan olarak seçili yapar
+
+        self.checkbox2 = QCheckBox('1 : Yükselme', self)
+        self.checkbox2.setGeometry(1030, 88, 170, 40)
+        self.checkbox2.setFont(QFont("Times New Roman", 14))
+
+        self.checkbox3 = QCheckBox('2 : Model Uydu İniş', self)
+        self.checkbox3.setGeometry(1030, 108, 170, 40)
+        self.checkbox3.setFont(QFont("Times New Roman", 14))
+
+        self.checkbox4 = QCheckBox('3 : Ayrılma', self)
+        self.checkbox4.setGeometry(1030, 128, 170, 40)
+        self.checkbox4.setFont(QFont("Times New Roman", 14))
+
+        self.checkbox5 = QCheckBox('4 : Görev Yükü İniş', self)
+        self.checkbox5.setGeometry(1030, 148, 170, 40)
+        self.checkbox5.setFont(QFont("Times New Roman", 14))
+        
+        self.checkbox6 = QCheckBox('5 : Kurtarma', self)
+        self.checkbox6.setGeometry(1030, 168, 170, 40)
+        self.checkbox6.setFont(QFont("Times New Roman", 14))
+    
+
         # Tablo
         self.dataTable = QTableView(self)
         self.dataTable.setGeometry(20, 650, 1300, 230)
-        self.setup_table()
+        self.setupDataTable()
+
+        self.ArayuzAlarmTable = QTableView(self)
+        self.ArayuzAlarmTable.setGeometry(1270, 15, 197, 50)
+        self.setupArayuzAlarmTable()
+
+        ArayuzAlarmString = "Hata Kodu: "
+        self.MissionTimeLabel = QLabel(self)
+        self.MissionTimeLabel.setGeometry(1285, 85, 170, 30)
+        self.MissionTimeLabel.setFont(QFont('Arial', 12))
+        self.MissionTimeLabel.setText(ArayuzAlarmString + "<" +"00000" + ">")
+        self.MissionTimeLabel.setStyleSheet("background-color: white")
 
         # Resimler
         LogoImage_PixMap = QPixmap("Taslak\Logo-Image.png")
@@ -130,12 +180,12 @@ class MainWindow(QMainWindow):
         AxisImage_PixMap = QPixmap("Taslak\Eksen-Image.png")
         AxisImage_Label = QLabel(self)
         AxisImage_Label.setPixmap(AxisImage_PixMap)
-        AxisImage_Label.setGeometry(1000, 0, AxisImage_PixMap.width(), AxisImage_PixMap.height())
+        AxisImage_Label.setGeometry(1270, 237, AxisImage_PixMap.width(), AxisImage_PixMap.height())
 
         CameraImage_PixMap = QPixmap("Taslak\Camera-Image.png")
         CameraImage_Label = QLabel(self)
         CameraImage_Label.setPixmap(CameraImage_PixMap)
-        CameraImage_Label.setGeometry(1440, 200, CameraImage_PixMap.width(), CameraImage_PixMap.height())
+        CameraImage_Label.setGeometry(1500, 200, CameraImage_PixMap.width(), CameraImage_PixMap.height())
 
         # Plot
         self.AltitudePlot = pg.PlotWidget(self)
@@ -163,13 +213,42 @@ class MainWindow(QMainWindow):
         self.dfg.setGeometry(950, 450, 270, 180)
         
 
-    def setup_table(self):
+    def setupDataTable(self):
         model = QStandardItemModel(7, 22)
         model.setHorizontalHeaderLabels([f"Kolon {i}" for i in range(1, 23)])
         model.setVerticalHeaderLabels([f"Satır {i}" for i in range(1, 8)])
 
         self.dataTable.setModel(model)
         self.dataTable.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
+
+    def setupArayuzAlarmTable(self):
+        # Model oluştur
+        model = QStandardItemModel(2, 5)
+
+        # Başlık satırını gizle
+        self.ArayuzAlarmTable.verticalHeader().hide()
+        # Başlık sütununu gizle
+        self.ArayuzAlarmTable.horizontalHeader().hide()
+
+        # Tabloya değerler ekle
+        for column in range(5):
+            item = QStandardItem(str(column + 1))
+            item.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
+            model.setItem(0, column, item)
+
+        # Alt satırı yeşil dolguyla doldur
+        for column in range(5):
+            item = QStandardItem("")
+            item.setBackground(QColor("green"))
+            model.setItem(1, column, item)
+
+        # Tabloya modeli ata
+        self.ArayuzAlarmTable.setModel(model)
+
+        # Hücre boyutlarını otomatik ayarla
+        self.ArayuzAlarmTable.resizeColumnsToContents()
+        self.ArayuzAlarmTable.resizeRowsToContents()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
